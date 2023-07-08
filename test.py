37e39a52b236fe0a51cec59e2b110987de411439
tklord0607 <136140384+tklord0607@users.noreply.github.com>
2023-07-08 07:53:54 +0000

    p1

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-#import random
+import random
 
 min = 1
 max = 100
 c=0
 t=random.randint(min,max)
 print("====???====\n\n")
-t=random1~10隨機產生，c=0 當k=t時遊戲結束，k由玩家輸入數值
+#t=random1~10隨機產生，c=0 當k=t時遊戲結束，k由玩家輸入數值
 #在while的區塊下 每執行一次c由0+1累計
 while True:
     k=int(input(f"??範圍{min}~~{max}:"))
