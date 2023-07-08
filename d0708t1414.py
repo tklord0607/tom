53087208febe2eed589be8aaf5be1185b2ae2eea
tklord0607 <136140384+tklord0607@users.noreply.github.com>
2023-07-08 06:42:08 +0000

    g1

--- d0708t1414.py
+++ d0708t1414.py
@@ -3,6 +3,19 @@
 import random
 
 min = 1
-max = 100
+max = 10
+c=0
 t=random.randint(min,max)
-print(t)
+print("====???====\n\n")
+
+while True:
+    k=int(input(f"??範圍{min}~~{max}:"))
+    c +=1
+    if(k == t):
+        print(f"Yes,正確:{t}")
+        print(f"你共猜了{c}次")
+        break
+    else:
+        print("NO")
+        print(f"你已猜了{c}次")
+print("GG")
