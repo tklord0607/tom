92061ce8d8473206963abfe6440182c16b9ffce8
tklord0607 <136140384+tklord0607@users.noreply.github.com>
2023-08-05 03:44:43 +0000

    d

--- T080503.py
+++ T080503.py
@@ -1,15 +1,18 @@
+#安裝網頁顯示用的模組 pip instaill streamlit
 import numpy as np
 import matplotlib.pyplot as plt
-
+import streamlit as st
 
 t = np.arange(0.,1.0,0.05)
-print(t)
+st.write(t)
 y1 = np.sin(2 * np.pi * t)
 y2 = np.cos(2 * np.pi * t)
-print(y1)
-print(y2)
+st.write(y1)
+st.write(y2)
 figure1 = plt.figure(figsize=(8,4))
 axes1 = figure1.add_subplot()
 axes1.plot(y1)
 axes1.plot(y2)
-plt.show()
+st.write(figure1)
+#在終端機輸入：streamlit run T080503.py   可用網頁顯示輸出資料與圖型
+#要離開按ctrl+c
